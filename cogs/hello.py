1f3379ba4d3da6987c512b056cc768d7c56c988d
Xin Yu <chuahxinyu2@gmail.com>
2022-08-13 17:37:53 +1000

    Update W1

--- cogs/hello.py
+++ cogs/hello.py
@@ -1,16 +1,18 @@
 from discord.ext import commands
 
-class Test(commands.Cog):
+class Hello(commands.Cog):
     def __init__(self, client):
         self.client = client # defining bot as global var in class
 
     @commands.Cog.listener() # this is a decorator for events/listeners
     async def on_ready(self):
         print(f'We have logged in as {self.client.user}')
+        
 
     @commands.command() # this is for making a command
-    async def ping(self, ctx):
-        await ctx.send(f'Pong! {round(self.bot.latency * 1000)}')
+    async def hello(self, ctx):
+        print(ctx)
+        await ctx.send(f'Hi!')
         
 def setup(bot): # a extension must have a setup function
-	bot.add_cog(Test(bot)) # adding a cog
+	bot.add_cog(Hello(bot)) # adding a cog
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# main.py
-from discord.ext import commands
+import discord
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-client = commands.Bot(command_prefix = "!")
+client = discord.Client()
 
-# Looks inside the /cogs/ folder and loads up all of our cogs
-for filename in os.listdir("./cogs"):
-	if filename.endswith(".py"):
-		client.load_extension("cogs." + filename[:-3])
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(msg):
+    # print(msg.content)
+    if msg.author == client.user:
+        return
+    if msg.content.startswith('$hello'):
+        await msg.channel.send(f'Hello, {msg.author.name}!')
 
 client.run(TOKEN)
